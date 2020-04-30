41df4dddd9ef84310c91ee9396577c0c21c069a7
Arch-Jason <arch-jason@outlook.com>
2020-04-30 21:54:41 +0800

    first commit

---
+++ autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# download
+/home/jason/myphoto/bing
+# set
+feh --bg-fill ~/myphoto/1039929-download-studio-ghibli-wallpaper-1920x1080-for-htc.jpg
+#set bar
+while true
+do
+    xsetroot -name "  $(date)  "
+    sleep 2
+done
